78f8089aa3cab138ee700eb401b8a73bd8d06c2a
matheus.figueredo@gmail.com <Matheus Figueredo>
2023-08-07 11:48:43 +0100

    Fixed conflict between JPA and Lombok

--- src/main/java/com/matheusfig90/entity/Booking.java
+++ src/main/java/com/matheusfig90/entity/Booking.java
@@ -7,13 +7,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Builder
 @Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "bookings")
 public class Booking {
--- src/main/java/com/matheusfig90/entity/Device.java
+++ src/main/java/com/matheusfig90/entity/Device.java
@@ -6,11 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "devices")
 public class Device {
