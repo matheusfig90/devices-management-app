9ef405d4b8b3e914c9044db5ec707343aa17f34b
matheus.figueredo@gmail.com <Matheus Figueredo>
2023-08-07 11:52:21 +0100

    Setup database

---
+++ src/main/resources/db/migration/V1__devices.sql
@@ -0,0 +1,11 @@
+INSERT INTO "devices" (id, name) VALUES
+    (1, 'Samsung Galaxy S9'),
+    (2, 'Samsung Galaxy S8'),
+    (3, 'Samsung Galaxy S8'),
+    (4, 'Motorola Nexus 6'),
+    (5, 'Oneplus 9'),
+    (6, 'Apple iPhone 13'),
+    (7, 'Apple iPhone 12'),
+    (8, 'Apple iPhone 11'),
+    (9, 'iPhone X'),
+    (10, 'Nokia 3310');
---
+++ src/main/resources/db/migration/V2__users.sql
@@ -0,0 +1,3 @@
+INSERT INTO "users" (id, name) VALUES
+    (1, 'Luis Suárez'),
+    (2, 'Juan Román Riquelme');
